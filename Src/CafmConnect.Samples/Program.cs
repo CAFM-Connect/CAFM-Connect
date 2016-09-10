9cb0eb8318735ea92e810206cdff529d6dcbf9fe
klacol <klaus.aengenvoort@etask.de>
2016-09-10 10:55:48 +0200

    Initial Commit aof CafmConnect.sln

---
+++ Src/CafmConnect.Samples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CafmConnect;
+
+namespace Ifc.Net.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Workspace cc = new Workspace();
+
+            string filename = @"c:\\tmp\\MyFirstEmptyCafmConnectFile.ifcxml";
+            cc.CreateFile(filename);
+
+        }
+    }
+}
---
+++ Src/CafmConnect/Workspace.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CafmConnect
+{
+    public class Workspace
+    {
+
+        public Workspace()
+        {
+
+        }
+
+        public void CreateFile(string filename)
+        { 
+            // ------------------------------------------------------
+            // Please work every time withe the catalogue template file
+            // Check the Original Source: http://katalog.cafm-connect.org/CC-Katalog/CAFM-ConnectFacilitiesViewTemplate.ifcxml
+            //-------------------------------------------------------
+
+            string tempPath = Path.GetTempPath();
+            string tempName = Guid.NewGuid().ToString();
+            using (var resource = Assembly.GetExecutingAssembly().GetManifestResourceStream("CafmConnect.Catalogue.CAFM-ConnectFacilitiesViewTemplate.ifcxml"))
+            {
+                using (var file = new FileStream(Path.Combine(tempPath, tempName), FileMode.Create, FileAccess.Write))
+                {
+                    resource.CopyTo(file);
+                }
+            }
+
+            Ifc.NET.Document ifc4Document = null;
+            ifc4Document = Ifc.NET.Workspace.CurrentWorkspace.OpenDocument(Path.Combine(tempPath, tempName));
+            ifc4Document.PopulateDefaultUosHeader();
+            ifc4Document.PopulateIndividualUosHeader("MyName","MyCompany","MySoftware","MyAuthorization");
+            ifc4Document.Workspace.SaveDocument(filename);
+        }
+
+    }
+}
